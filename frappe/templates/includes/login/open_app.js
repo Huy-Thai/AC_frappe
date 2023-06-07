c88e84992103a5d0a7b1417bddf6c35aa0b8d8ef
Huy-Thai <huythai.dev@gmail.com>
2023-06-07 14:24:25 +0700

    update page open app

--- frappe/templates/includes/login/open_app.js
+++ frappe/templates/includes/login/open_app.js
@@ -1,6 +1,8 @@
-frappe.ready(function () {
-	window.open(
-        'acerp-desk-app-dev://res=openapp',
-        '_blank',
-    )
+frappe.ready(() => {
+	$(window).on("load", (_) => {
+		window.open(
+            'acerp-desk-app-dev://res=openapp',
+            '_blank',
+        ).focus();
+	});
 });
--- frappe/www/open_app.py
+++ frappe/www/open_app.py
@@ -8,9 +8,5 @@ def get_context(context):
 	context["title"] = "Open ERP Desktop App"
 	context["hide_login"] = True
 	context["user_context"] = frappe.session
-	context["logo"] = frappe.get_website_settings("app_logo") or frappe.get_hooks("app_logo_url")[-1]
-	context["app_name"] = (
-		frappe.get_website_settings("app_name") or frappe.get_system_settings("app_name") or _("Frappe")
-	)
 
 	return context
