d1357fbc26442b6e087a433e3a69ec100c8e8a50
Huy-Thai <huythai.dev@gmail.com>
2024-03-28 10:42:33 +0700

    update style form login

--- frappe/templates/includes/login/app_auth_res.js
+++ frappe/templates/includes/login/app_auth_res.js
@@ -1,5 +1,3 @@
-const CryptoJS = require('crypto-js');
-
 window.login = {};
 
 login.bind_events = function () {
@@ -9,12 +7,10 @@ login.bind_events = function () {
 
 	$(".form-login").on("submit", function (event) {
 		event.preventDefault();
-		const passphrase = 'ehb88S23us3XiaJWleYvPH2Bv8NgSIgp';
 		let cookies = "{{ user_context }}";
-		let encryptCookies = CryptoJS.AES.encrypt(cookies, passphrase).toString();
 
 		window.open(
-			`acerp-desk-app://res=${encryptCookies}`,
+			`acerp-desk-app://res=${cookies}`,
 			'_blank',
 		)
 
